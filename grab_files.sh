4750cd32d627dbed3d1fd7b7e704f799dff70196
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-04-09 17:23:03 -0500

    mexmaca64

--- grab_files.sh
+++ grab_files.sh
@@ -7,15 +7,15 @@ for f in \
 LICENCE.txt \
 spm_vol.m \
 spm_fileparts.m \
-spm_existfile.m* \
-spm_unlink.m* \
+spm_existfile.m \
+spm_unlink.m \
 spm_vol_nifti.m \
 \@nifti \
 \@file_array \
 spm_read_vols.m \
 spm_check_orientations.m \
 spm_mesh_detect.m \
-spm_slice_vol.m* \
+spm_slice_vol.m \
 spm_matrix.m \
 spm_type.m \
 spm_write_vol.m \
@@ -23,5 +23,5 @@ spm_create_vol.m \
 spm_file.m \
 spm_write_plane.m \
 ; do
-    cp -R spm12/$f .
+    cp -R spm12/${f}* .
 done
